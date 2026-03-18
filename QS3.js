da84d41efa5af099ec8decea1cb2e12035edd097
Md Alkama <mdalkamadad@gmail.com>
2026-03-18 09:56:38 +0530

    qs3 done

---
+++ QS3.js
@@ -0,0 +1,7 @@
+// Accept two integers from user and print the sum (Ex - The sum of 45 & 12 = 57)
+
+function sumWithMsg(a,b){
+    console.log(`The sum of ${a} & ${b} = ${a+b}`)
+}
+
+sumWithMsg(45,12)
