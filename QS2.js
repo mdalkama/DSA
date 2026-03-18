6fab9dfce297204dca3c45a3a9abca91e937b54a
Md Alkama <mdalkamadad@gmail.com>
2026-03-18 09:56:33 +0530

    qs2 edited

--- QS2.js
+++ QS2.js
@@ -1,7 +1,3 @@
 // Print the message along with values and sum (Ex - The sum of 45 & 12 = 57)
 
-function sumWithMsg(a,b){
-    console.log(`The sum of ${a} & ${b} = ${a+b}`)
-}
-
-sumWithMsg(45,12)
+    console.log(`The sum of ${45} & ${12} = ${45+12}`)
