43914c47b462d9168ff385d8ec0932403e50b790
Md Alkama <mdalkamadad@gmail.com>
2026-03-18 10:51:14 +0530

    qs15 done

---
+++ QS15.js
@@ -0,0 +1,11 @@
+// Accept an integer and check whether it is an even number or odd.
+
+
+
+function checkEvenOdd(num) {
+
+    return num % 2 == 0 ? "Even" : "Odd"
+
+}
+
+console.log(checkEvenOdd(8))
