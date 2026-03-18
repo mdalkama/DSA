18f97e672fb72d2c151100be8601b4dd3ee0eca9
Md Alkama <mdalkamadad@gmail.com>
2026-03-18 09:58:29 +0530

    qs4 edited

---
+++ QS4.js
@@ -0,0 +1,8 @@
+// Accept the User's name, age and print in following manner (Ex - Hello Shery, you are 12 years old.)
+
+
+function printNameAndAge(name, age){
+    console.log(`Hello ${name}, you are ${age} years old.`)
+}
+
+printNameAndAge("alkama", 22)
