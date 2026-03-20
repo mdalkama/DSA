da4171524036e9fbe571ecb4e7f435fc29c1411f
Md Alkama <mdalkamadad@gmail.com>
2026-03-20 17:47:09 +0530

    qs 49 edit

--- QS49.js
+++ QS49.js
@@ -1,11 +1,11 @@
 // Mirrored Right Triangle Pattern
 
-function mirrorStr(num){
+function mirrorStr(num) {
     let val = ""
-    for(let i = 1; i<=num; i++){
-        val = " ".repeat(num-i)+"*".repeat(i)
+    for (let i = 1; i <= num; i++) {
+        val = " ".repeat(num - i) + "*".repeat(i)
         console.log(val)
-        val=""
+        val = ""
     }
 }
 mirrorStr(5)
