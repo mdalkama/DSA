0c7f43548054e0d18dc03e60f6cca35b7ef9dbe4
Md Alkama <mdalkamadad@gmail.com>
2026-03-20 10:18:11 +0530

    qs23 done

---
+++ QS23.js
@@ -0,0 +1,9 @@
+// Accept an integer and Print \Hello World\" n times"
+
+function printHelloWorld(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("Hello World")
+    }
+}
+
+printHelloWorld(10)
