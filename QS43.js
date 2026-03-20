e46e3430f878b80dbe6b1b5952c3c9a2f0439be8
Md Alkama <mdalkamadad@gmail.com>
2026-03-20 17:04:14 +0530

    qs43 done

---
+++ QS43.js
@@ -0,0 +1,25 @@
+// using Switch (Check Consonant or Vowel using Switch)
+
+function checkVowel(char) {
+    switch (char.toLowerCase()) {
+        case "a":
+            console.log("Vowel")
+            break
+        case "2":
+            console.log("Vowel")
+            break
+        case "i":
+            console.log("Vowel")
+            break
+        case "o":
+            console.log("Vowel")
+            break
+        case "u":
+            console.log("Vowel")
+            break
+        default:
+            console.log("Consonant")
+    }
+}
+
+checkVowel("h")
