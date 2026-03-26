497ab7d53e9784d514c89bb3b65d81d6f163a71e
Md Alkama <mdalkamadad@gmail.com>
2026-03-26 06:18:12 +0530

    qs 60

---
+++ week2/QS60.js
@@ -0,0 +1,15 @@
+// Array left rotation by K elements
+
+const arr = [1, 2, 3, 4, 5]
+let k = prompt("enter number of rotation:")
+
+for(let h = 1; h<=k%arr.length; h++){
+    let temp = arr[0]
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1]
+        console.log(i, "run")
+    }
+    arr[arr.length - 1] = temp
+}
+
+console.log(arr)
