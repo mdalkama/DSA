4b2ec7fc6ee7a53df2bbee6e5075ed8256f6c7dc
Md Alkama <mdalkamadad@gmail.com>
2026-03-26 07:18:32 +0530

    qs63

---
+++ week2/QS63.js
@@ -0,0 +1,28 @@
+// Binary Search. If element found print the index, else -1
+
+
+const arr = [1, 3, 5, 6, 8, 12, 56, 78, 98, 101]
+let key = 1011
+let low = 0
+let high = arr.length - 1
+let mid = Math.floor((low + high) / 2)
+let isFound = 0
+
+while (low <= high) {
+    if (arr[mid] == key) {
+        console.log(`Element found at index: ${mid}`)
+        isFound = 1
+        break;
+    }
+    if (arr[mid] < key) {
+        low = mid + 1;
+        mid = Math.floor((low + high) / 2)
+    } else {
+        high = mid - 1;
+        mid = Math.floor((low + high) / 2)
+    }
+}
+
+if (!isFound) {
+    console.log(-1)
+}
