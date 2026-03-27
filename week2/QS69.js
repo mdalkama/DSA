bdce0f02bcbfa30d51be9ac52ad3f006709a203c
Md Alkama <mdalkamadad@gmail.com>
2026-03-27 18:48:43 +0530

    2nd method

--- week2/QS69.js
+++ week2/QS69.js
@@ -2,13 +2,5 @@
 
 const arr = [1, 2, 3, 4]
 const arr2 = [5, 6, 7, 8]
-
-const finalArray = new Array(arr.length + arr2.length)
-
-for (let i = 0; i < arr.length; i++) {
-    finalArray[i] = arr[i]
-}
-for (let i = arr.length; i < arr.length + arr2.length; i++) {
-    finalArray[i] = arr2[i - arr.length]
-}
+const finalArray = [...arr, ...arr2]
 console.log(finalArray)
