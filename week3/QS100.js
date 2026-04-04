ceecaa6be40d45129597f3fd7d1e3d0c70ee737c
Md Alkama <mdalkamadad@gmail.com>
2026-04-04 21:15:14 +0530

    qs100 done

---
+++ week3/QS100.js
@@ -0,0 +1,8 @@
+// Accept a string from user and print its each character on a new line
+
+
+const str = prompt("enter a string: ")
+
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i])
+}
