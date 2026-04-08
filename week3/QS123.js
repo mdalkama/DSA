4308bbea3fc51e63324ff1b72a9e121b1e33118e
Md Alkama <mdalkamadad@gmail.com>
2026-04-08 18:38:20 +0530

    123 done

--- week3/QS123.js
+++ week3/QS123.js
@@ -1,22 +1,41 @@
-// Calculate Digit Sum of a String
+// Find Resultant Array After Removing Anagrams
 
-let s = "3465"
-let k = 3
-let newStr = s
+const words = ["abba", "baba", "bbaa", "cd", "cd"];
+const result = [];
 
-while (newStr.length > k) {
-    let str = ""
-    let i = 0
-    while (i < newStr.length) {
-        let j = 0
-        let num = 0
-        while (j < k && i < newStr.length) {
-            num += +newStr[i]
-            j++
-            i++
+result.push(words[0]);
+
+let i = 0;
+let j = 1;
+
+while (j < words.length) {
+
+    let isAnagram = true;
+
+    if (words[i].length === words[j].length) {
+        let imap = {};
+        let jmap = {};
+
+        for (let k = 0; k < words[i].length; k++) {
+            imap[words[i][k]] = (imap[words[i][k]] || 0) + 1;
+            jmap[words[j][k]] = (jmap[words[j][k]] || 0) + 1;
         }
-        str += num
+
+        for (let key in imap) {
+            if (imap[key] !== jmap[key]) {
+                isAnagram = false;
+                break;
+            }
+        }
+    } else {
+        isAnagram = false;
     }
-    newStr = str
+
+    if (!isAnagram) {
+        result.push(words[j]);
+        i = j;
+    }
+    j++;
 }
-console.log(newStr)
+
+console.log(result);
