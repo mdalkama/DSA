849a8450fddd32082a9e43e9d87313bd164a8401
Md Alkama <mdalkamadad@gmail.com>
2026-04-11 17:53:48 +0530

    qs 134 done

---
+++ week3/QS134.js
@@ -0,0 +1,17 @@
+// Replace Elements with Greatest Element on Right Side
+
+const arr = [400]
+
+
+for (let i = 0; i < arr.length - 1; i++) {
+    let max = arr[i+1]
+    for (let j = i + 1; j < arr.length; j++){
+        if(arr[j] > max){
+            max = arr[j]
+        }
+        arr[i] = max
+    }
+}
+arr[arr.length - 1] = -1
+
+console.log(arr)
