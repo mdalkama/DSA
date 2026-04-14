40a8247945662fe76708f6ae493e50dabe5a0a72
Md Alkama <mdalkamadad@gmail.com>
2026-04-14 16:26:16 +0530

    96 done

--- week3/QS96.js
+++ week3/QS96.js
@@ -1,14 +1,33 @@
-const arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+// Valid Sudoku
 
+const board =
+    [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+        , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+        , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+        , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+        , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+        , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+        , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+        , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+        , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 
-for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-        [arr[i][j], arr[j][i]] = [arr[j][i], arr[i][j]]
+const rows = Array.from({ length: 9 }, () => Array(9).fill(false));
+const cols = Array.from({ length: 9 }, () => Array(9).fill(false));
+const boxes = Array.from({ length: 9 }, () => Array(9).fill(false));
+
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        if (board[i][j] !== '.') {
+            let num = board[i][j].charCodeAt(0) - '1'.charCodeAt(0);
+            let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+            if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                console.log(false)
+            }
+
+            rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+        }
     }
 }
 
-for (let i = 0; i < arr.length; i++) {
-    arr[i].reverse()
-}
-
-console.log(arr)
+console.log(true)
