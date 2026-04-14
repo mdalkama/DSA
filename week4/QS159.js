cf9f56e22682403ca64485c2969a6861b4063527
Md Alkama <mdalkamadad@gmail.com>
2026-04-14 23:35:02 +0530

    159 done

---
+++ week4/QS159.js
@@ -0,0 +1,20 @@
+// 
+
+const nums = [0, 1, 2, 2, 4, 4, 1]
+
+let freq = {}
+
+for (let i of nums) {
+    if (i % 2 == 0) {
+        freq[i] = (freq[i] || 0) + 1
+    }
+}
+let elem = -1
+let max = 0
+for (let i in freq) {
+    if (freq[i] > max) {
+        elem = i
+        max = freq[i]
+    }
+}
+console.log(max)
