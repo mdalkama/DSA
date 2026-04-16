1bd2970fe2d25f48fee8d6854fd27f3fbf5d407f
Md Alkama <mdalkamadad@gmail.com>
2026-04-16 13:49:42 +0530

    166 done

---
+++ week4/QS166.js
@@ -0,0 +1,22 @@
+// Set Mismatch
+
+const nums = [1, 2, 2, 4]
+
+const n = nums.length;
+const v = new Array(n + 1).fill(0);
+let missing = 0, duplicate = 0;
+
+for (const num of nums) {
+    v[num]++;
+}
+
+for (let i = 1; i < v.length; i++) {
+    if (v[i] === 2) {
+        duplicate = i;
+    }
+    if (v[i] === 0) {
+        missing = i;
+    }
+}
+
+console.log([duplicate, missing])
