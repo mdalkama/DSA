82954e367de8ed66a0bb01906c1b6646b4d07817
Md Alkama <mdalkamadad@gmail.com>
2026-04-19 11:41:55 +0530

    181 done

---
+++ week4/QS181.js
@@ -0,0 +1,18 @@
+// Floor of a number in a sorted array
+
+
+const nums = [1, 3, 5, 8, 10, 12, 15, 18, 20]
+const x = 5;
+
+
+let answer = -1;
+
+for (let i of nums) {
+    if (i <= x) {
+        answer = i;
+    } else {
+        break;
+    }
+}
+
+console.log(answer);
