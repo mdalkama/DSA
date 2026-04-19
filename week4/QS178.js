3872d147c12640767bff874f06e5f00dc45daac3
Md Alkama <mdalkamadad@gmail.com>
2026-04-19 11:30:23 +0530

    178 done

---
+++ week4/QS178.js
@@ -0,0 +1,12 @@
+// Guess Number Higher or Lower
+
+// i have solved this question inside leetcode 
+
+let left = 1, right = n;
+while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let res = guess(mid);
+    if (res === 0) return mid;
+    else if (res === -1) right = mid - 1;
+    else left = mid + 1;
+}
