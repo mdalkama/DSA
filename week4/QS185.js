04d356b4344bd1503eb82982f806543bac61eb93
Md Alkama <mdalkamadad@gmail.com>
2026-04-20 16:07:00 +0530

    edited with different way

--- week4/QS185.js
+++ week4/QS185.js
@@ -5,20 +5,37 @@ const nums = [5, 7, 7, 8, 8, 10]
 const target = 8
 
 function binary(nums, target, findFirst) {
-    let s = 0, e = nums.length - 1, ans = -1;
-    while (s <= e) {
-        const mid = Math.floor((s + e) / 2);
-        if (target > nums[mid]) {
-            s = mid + 1;
-        } else if (target < nums[mid]) {
-            e = mid - 1;
-        } else {
-            ans = mid;
-            if (findFirst) e = mid - 1;
-            else s = mid + 1;
+    let result = -1
+
+    let low = 0;
+    let high = nums.length - 1
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        if (nums[mid] < target) {
+            low = mid + 1
+        }
+        else if (nums[mid] > target) {
+            high = mid - 1
+        }
+        else {
+            result = mid
+
+            if (findFirst) {
+                while (nums[mid - 1] == target) {
+                    result = mid - 1
+                    mid--
+                }
+            } else {
+                while (nums[mid + 1] == target) {
+                    result = mid + 1
+                    mid++
+                }
+            }
+            break;
         }
     }
-    return ans;
+    return result;
 }
 
 console.log([binary(nums, target, true), binary(nums, target, false)])
