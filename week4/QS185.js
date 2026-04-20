3b25bcc898d3bb30e16395c9fdb0c9507c427804
Md Alkama <mdalkamadad@gmail.com>
2026-04-20 15:27:11 +0530

    185 done

---
+++ week4/QS185.js
@@ -0,0 +1,24 @@
+// Find First and Last Position of Element in Sorted Array
+
+
+const nums = [5, 7, 7, 8, 8, 10]
+const target = 8
+
+function binary(nums, target, findFirst) {
+    let s = 0, e = nums.length - 1, ans = -1;
+    while (s <= e) {
+        const mid = Math.floor((s + e) / 2);
+        if (target > nums[mid]) {
+            s = mid + 1;
+        } else if (target < nums[mid]) {
+            e = mid - 1;
+        } else {
+            ans = mid;
+            if (findFirst) e = mid - 1;
+            else s = mid + 1;
+        }
+    }
+    return ans;
+}
+
+console.log([binary(nums, target, true), binary(nums, target, false)])
