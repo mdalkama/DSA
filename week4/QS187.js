1a3a35a2b64b1ddd39a7573e38dbd3b3393cc79f
Md Alkama <mdalkamadad@gmail.com>
2026-04-20 16:26:07 +0530

    187  dn

---
+++ week4/QS187.js
@@ -0,0 +1,19 @@
+// Peak Index in a Mountain Array / Find maximum element in bitonic array (Monotonic, Bitonic Array)
+
+const nums = [0, 10, 5, 2]
+
+
+let low = 0;
+let high = nums.length - 1;
+
+while (low < high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] < nums[mid + 1]) {
+        low = mid + 1;
+    } else {
+        high = mid;
+    }
+}
+
+console.log(low)
