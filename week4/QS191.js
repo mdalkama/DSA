0bb9f5bf070356396fcb2f3e4b832ecd3deb5eaf
Md Alkama <mdalkamadad@gmail.com>
2026-04-24 10:51:27 +0530

    191 done

--- week4/QS191.js
+++ week4/QS191.js
@@ -4,11 +4,10 @@
 const arr = [1, 2, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 45, 76, 84, 89, 94, 98, 102, 116, 117, 124, 130, 140, 147, 152, 159, 172, 165, 167, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191]
 
 const target = 124
+let op = 0
 
-function bindarySearch(nums, exp) {
-
-    let left = 0;
-    let right = exp;
+function bindarySearch(nums, left, right) {
+op++
     let flag = -1
 
     while (left <= right) {
@@ -34,14 +33,15 @@ function bindarySearch(nums, exp) {
     return flag
 }
 
+let low = 0
+let high = 2
 
-
-let exp = 2
-
-while (target > arr[exp]) {
-    exp *= 2
+while (target > arr[high]) {
+    op++
+    low = high
+    high *= 2
 }
 
+console.log(bindarySearch(arr, low, high))
 
-console.log(bindarySearch(arr, exp))
-
+console.log(op)
